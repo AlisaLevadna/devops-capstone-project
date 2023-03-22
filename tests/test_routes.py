cde18cd29b7f0c6375634ae687d4a0eb669274a0
AlisaLevadna <alisa.levadna@nure.ua>
2023-03-21 20:49:18 -0400

    add update account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -145,4 +145,32 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/0", content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        
+
+    def test_update_an_account(self):
+        """It should Update an existing Account"""
+        test_account = AccountFactory()
+        response = self.client.post(
+            BASE_URL, json=test_account.serialize()
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        new_account = response.get_json()
+        new_account["name"] = "New Name"
+        response = self.client.put(
+            f"{BASE_URL}/{new_account['id']}", json=new_account
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "New Name")
+    
+    def test_update_nonexisting_account(self):
+        """It should Update an existing Account"""
+        test_account = AccountFactory()
+        test_account.id = 0
+        response = self.client.put(
+            f"{BASE_URL}/{test_account.id}", json=test_account.serialize()
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
